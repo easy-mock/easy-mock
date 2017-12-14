568f6451be43c884cb4d2392d280ef9fa75f4c7c
chuangker <chuangker@foxmail.com>
2017-12-15 01:49:50 +0800

    refactor(server): optimize logger

--- util/logger.js
+++ util/logger.js
@@ -5,30 +5,28 @@ const uuid = require('uuid')
 const path = require('path')
 const mkdirp = require('mkdirp')
 const moment = require('moment')
-const pinoTee = require('pino-tee')
 const koaPinoLogger = require('koa-pino-logger')
+const multistream = require('pino-multi-stream').multistream
 
-const stream = pinoTee(process.stdin)
 const date = moment().format('YYYY-MM-DD')
 const logDir = path.join(__dirname, '../logs')
-const streamOpt = {flags: 'a', encoding: 'utf8'}
-const isProd = process.env.NODE_ENV === 'production'
 const isTest = process.env.NODE_ENV === 'test'
+let streams = [
+  {level: 'info', stream: process.stdout},
+  {level: 'error', stream: process.stderr}
+]
 
-if (isProd) {
+if (process.env.NODE_ENV === 'production') {
   if (!fs.existsSync(logDir)) mkdirp.sync(logDir)
 
-  const errorStream = fs.createWriteStream(`logs/${date}-error.log`, streamOpt)
-  const infoStream = fs.createWriteStream(`logs/${date}-info.log`, streamOpt)
-
-  stream.tee(errorStream, line => line.level >= 50)
-  stream.tee(infoStream, line => line.level >= 30 && line.level < 50)
-  stream.pipe(process.stdout)
+  streams = [
+    {level: 'info', stream: fs.createWriteStream(`logs/${date}-info.log`)},
+    {level: 'error', stream: fs.createWriteStream(`logs/${date}-error.log`)}
+  ]
 }
 
 module.exports = koaPinoLogger({
   name: 'Easy Mock',
-  prettyPrint: !isProd,
   level: isTest ? 'error' : 'info',
   genReqId: req => req.headers['x-request-id'] || uuid.v4()
-}, isProd ? stream : process.stdout)
+}, multistream(streams))
